651c4b9f646b3f2fb5cf5a5f2ff4fa8c826a9d32
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2024-10-26 00:29:14 -0400

    Reorganize

--- protohell.py
+++ protohell.py
@@ -1,4 +1,3 @@
-import base64
 import dataclasses
 import datetime
 import enum
@@ -14,6 +13,7 @@ import asn1
 import betterproto
 import requests
 import rich
+import urllib3
 from google.protobuf import struct_pb2
 from google.protobuf.json_format import MessageToDict
 
@@ -34,63 +34,131 @@ from lib import (
     NodeType,
     ProtocolVersion,
     ReleaseMetadata,
+    ReleaseMetadataSchemaVersion,
 )
 
-SAVE_DIR = Path("releases")
-SAVE_DIR.mkdir(exist_ok=True)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+VERBOSE = True
+
+# Save responses to local cache
+SAVE_TO_LOCAL_CACHE = True
+# Save JSON conversions to local cache. Independent of SAVE_TO_LOCAL_CACHE
+SAVE_JSON_TO_LOCAL_CACHE = True
+
+# Use local cache instead of fetching from server. Useful for parsing a dump.
+LOAD_FROM_LOCAL_CACHE = False
+
+# Serialize enums as strings instead of integers
+SERIALIZE_AS_ENUM = True
+
+# Only emit release-metadata.json files, no other release information
+ONLY_RELEASE_METADATA = False
+
+ROOT_DIR = Path("data")
+TREES_DIR = ROOT_DIR / Path("trees")
+RELEASES_DIR = ROOT_DIR / Path("releases")
 
 REQUEST_UUID = str(uuid.uuid4())
 
 SESSION = requests.Session()
 SESSION.verify = False
 SESSION.headers = {"X-Apple-Request-UUID": REQUEST_UUID, "Content-Type": "application/protobuf"}
+TIMEOUT = 10
 
 
 BAG_URL = "https://init-kt-prod.ess.apple.com/init/getBag?ix=5&p=atresearch"
-BAG = plistlib.loads(SESSION.get(BAG_URL, timeout=3).content)
+BAG = plistlib.loads(SESSION.get(BAG_URL, timeout=TIMEOUT).content)
 
 
-def get_trees():
+def write(path: Path, content: str | bytes):
+    path.parent.mkdir(exist_ok=True, parents=True)
+    if isinstance(content, str):
+        path.write_text(content)
+    else:
+        path.write_bytes(content)
+
+
+def save_to_local_cache(path: Path, content: bytes):
+    if SAVE_TO_LOCAL_CACHE:
+        # path.write_bytes(content)
+        write(path, content)
+
+
+def save_json_to_local_cache(path: Path, content: betterproto.Message):
+    if SAVE_JSON_TO_LOCAL_CACHE:
+        # path.write_text(content.to_json(indent=4))
+        write(path, content.to_json(indent=4))
+
+
+def fetch_trees():
+    if LOAD_FROM_LOCAL_CACHE:
+        return (TREES_DIR / "list_trees_response.binpb").read_bytes()
     body = ListTreesRequest(ProtocolVersion.V3, REQUEST_UUID)
 
     resp = SESSION.post(
         BAG["at-researcher-list-trees"],
         data=bytes(body),
-        timeout=5,
+        timeout=TIMEOUT,
     )
     resp.raise_for_status()
-    trees = ListTreesResponse().parse(resp.content)
-    rich.print(trees)
+    save_to_local_cache(TREES_DIR / "list_trees_response.binpb", resp.content)
+    return resp.content
+
+
+def get_trees():
+    raw = fetch_trees()
+    trees = ListTreesResponse().parse(raw)
+    save_json_to_local_cache(TREES_DIR / "list_trees_response.json", trees)
+
+    for tree in trees.trees:
+        save_json_to_local_cache(TREES_DIR / str(tree.tree_id) / "tree.json", tree)
 
     return trees
 
 
-def get_log_head_for_tree(tree: ListTreesResponseTree):
+def fetch_log_head_for_tree(tree: ListTreesResponseTree):
+    # Note: We save and load the log head rather than the signed object, as we do not need the signature
+    if LOAD_FROM_LOCAL_CACHE:
+        return (TREES_DIR / str(tree.tree_id) / "log_head.binpb").read_bytes()
     body = LogHeadRequest(ProtocolVersion.V3, tree.tree_id, -1, REQUEST_UUID)
     resp = SESSION.post(
         BAG["at-researcher-log-head"],
         data=bytes(body),
-        timeout=5,
+        timeout=TIMEOUT,
     )
     resp.raise_for_status()
     signed_log_head = LogHeadResponse().parse(resp.content)
-    rich.print(signed_log_head)
-    log_head = LogHead().parse(signed_log_head.log_head.object)
-    rich.print(log_head)
+    save_to_local_cache(TREES_DIR / str(tree.tree_id) / "log_head.binpb", signed_log_head.log_head.object)
+    return signed_log_head.log_head.object
+
+
+def get_log_head_for_tree(tree: ListTreesResponseTree):
+    raw = fetch_log_head_for_tree(tree)
+    log_head = LogHead().parse(raw)
+    save_json_to_local_cache(TREES_DIR / str(tree.tree_id) / "log_head.json", log_head)
 
     return log_head
 
 
-def get_log_leaves(tree: ListTreesResponseTree, start_index: int, end_index: int):
+def fetch_log_leaves(tree: ListTreesResponseTree, start_index: int, end_index: int):
+    if LOAD_FROM_LOCAL_CACHE:
+        return (TREES_DIR / str(tree.tree_id) / "log_leaves.binpb").read_bytes()
     body = LogLeavesRequest(ProtocolVersion.V3, tree.tree_id, start_index, end_index, REQUEST_UUID, 0, tree.merge_groups)
     resp = SESSION.post(
         BAG["at-researcher-log-leaves"],
         data=bytes(body),
-        timeout=5,
+        timeout=TIMEOUT,
     )
     resp.raise_for_status()
-    log_leaves = LogLeavesResponse().parse(resp.content)
-    # rich.print(log_leaves)
+    save_to_local_cache(TREES_DIR / str(tree.tree_id) / "log_leaves.binpb", resp.content)
+    return resp.content
+
+
+def get_log_leaves(tree: ListTreesResponseTree, start_index: int, end_index: int):
+    raw = fetch_log_leaves(tree, start_index, end_index)
+    log_leaves = LogLeavesResponse().parse(raw)
+    save_json_to_local_cache(TREES_DIR / str(tree.tree_id) / "log_leaves.json", log_leaves)
 
     return log_leaves
 
@@ -142,29 +210,35 @@ def parse_at_leaf(raw: bytes):
 
 @dataclasses.dataclass(init=False)
 class Release:
+    release_metadata_present: bool = False
+    schema: Optional[ReleaseMetadataSchemaVersion] = None
     index: int
-    created: datetime.datetime
-    expires: datetime.datetime
+    created: Optional[datetime.datetime] = None
+    expires: Optional[datetime.datetime] = None
     hash: Optional[bytes]
-    assets: list[dict]
+    assets: Optional[list[dict]] = None
     tickets_raw: bytes
     ap_ticket: bytes
     cryptex_tickets: list[bytes]
-    darwin_init: dict
+    darwin_init: Optional[dict] = None
 
     def __init__(self, log_leaf: LogLeavesResponseLeaf, at_leaf: ATLeaf) -> None:
         self.index = log_leaf.index
         self.expires = at_leaf.expiry
         self.hash = at_leaf.hash
-        release_metadata = ReleaseMetadata().parse(log_leaf.metadata)
-        self.created = release_metadata.timestamp
-        self.assets = [x.to_pydict() for x in release_metadata.assets]
-        self.darwin_init = MessageToDict(struct_pb2.Struct.FromString(bytes(release_metadata.darwin_init)))
+        if log_leaf.metadata:
+            self.release_metadata_present = True
+            release_metadata = ReleaseMetadata().parse(log_leaf.metadata)
+            self.schema = release_metadata.schema_version
+            self.created = release_metadata.timestamp
+            self.assets = [x.to_pydict() for x in release_metadata.assets]
+            self.darwin_init = MessageToDict(struct_pb2.Struct.FromString(bytes(release_metadata.darwin_init)))  # pylint: disable=no-member
 
         self.tickets_raw = log_leaf.raw_data
+        assert self.tickets_raw
 
         decoder = asn1.Decoder()
-        decoder.start(log_leaf.raw_data)
+        decoder.start(self.tickets_raw)
         tag = decoder.peek()
         assert tag.nr == asn1.Numbers.Sequence
         decoder.enter()
@@ -188,25 +262,7 @@ class Release:
             self.cryptex_tickets.append(cryptex_ticket)
 
 
-def get_releases():
-    trees = get_trees()
-
-    selected_tree = None
-    for tree in trees.trees:
-        if tree.application == Application.PRIVATE_CLOUD_COMPUTE and tree.log_type == LogType.AT_LOG:
-            assert selected_tree is None, "Multiple AT_LOG trees found"
-            selected_tree = tree
-
-    assert selected_tree is not None, "No AT_LOG tree found"
-    rich.print(selected_tree)
-
-    log_head = get_log_head_for_tree(selected_tree)
-
-    start_index = 0
-    end_index = log_head.log_size
-    print(f"Log size: {end_index}")
-    log_leaves = get_log_leaves(selected_tree, start_index, end_index)
-
+def get_releases_from_leaves(log_leaves: LogLeavesResponse):
     releases: list[Release] = []
 
     for log_leaf in log_leaves.leaves:
@@ -225,51 +281,96 @@ def get_releases():
     return releases
 
 
-def serializer(obj):
-    if isinstance(obj, (datetime.datetime, datetime.date)):
-        return obj.isoformat()
-    elif isinstance(obj, bytes):
-        # return base64.b64encode(obj).decode()
-        return obj.hex()
-    elif isinstance(obj, (enum.Enum, betterproto.Enum)):
-        assert False
+def convert_enum_to_name(obj):
+    if not SERIALIZE_AS_ENUM:
+        return obj
+
+    if isinstance(obj, (enum.Enum, betterproto.Enum)):
         return obj.name
-    # elif isinstance(obj, asn1.Object):
-    #     return obj.dump()
+    elif isinstance(obj, list):
+        return [convert_enum_to_name(x) for x in obj]
+    elif isinstance(obj, dict):
+        return {k: convert_enum_to_name(v) for k, v in obj.items()}
     else:
         return obj
 
 
+class ReleaseEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, (datetime.datetime, datetime.date)):
+            return o.isoformat()
+        elif isinstance(o, bytes):
+            return o.hex()
+        else:
+            return super().default(o)
+
+
 if __name__ == "__main__":
-    releases = get_releases()
+    releases = []
+
+    trees = get_trees()
+
+    for tree in trees.trees:
+
+        log_head = get_log_head_for_tree(tree)
+
+        start_index = 0
+        end_index = log_head.log_size
+        log_leaves = get_log_leaves(tree, start_index, end_index)
+
+        if tree.application == Application.PRIVATE_CLOUD_COMPUTE and tree.log_type == LogType.AT_LOG:
+            releases = get_releases_from_leaves(log_leaves)
 
     for release in releases:
-        rich.print(release)
-        release_dir = SAVE_DIR / f"{release.index}"
-        release_dir.mkdir(exist_ok=True)
-        with (release_dir / "description.txt").open("w") as f:
-            rich.print(release, file=f)
-        (release_dir / "metadata.json").write_text(
-            json.dumps(
-                {i: v for i, v in dataclasses.asdict(release).items() if i in ["index", "created", "expires", "hash"]}
-                | {
-                    "tickets": {
-                        "os": hashlib.sha256(release.ap_ticket).hexdigest(),
-                        "cryptexes": [hashlib.sha256(x).hexdigest() for x in release.cryptex_tickets],
-                    }
-                },
-                indent=4,
-                default=serializer,
-            )
-        )
-        if release.assets:
-            (release_dir / "assets.json").write_text(json.dumps(release.assets, indent=4, default=serializer))
-        if release.darwin_init:
-            (release_dir / "darwin_init.json").write_text(json.dumps(release.darwin_init, indent=4, default=serializer))
-        (release_dir / "tickets_raw.der").write_bytes(release.tickets_raw)
-        (release_dir / "apticket.der").write_bytes(release.ap_ticket)
+        if VERBOSE:
+            rich.print(release)
 
-        cryptex_tickets_dir = release_dir / "cryptex_tickets"
-        cryptex_tickets_dir.mkdir(exist_ok=True)
-        for i, ticket in enumerate(release.cryptex_tickets):
-            (cryptex_tickets_dir / f"cryptex_ticket_{i}.der").write_bytes(ticket)
+        release_dir = RELEASES_DIR / f"{release.index}"
+        if not ONLY_RELEASE_METADATA:
+            write(
+                release_dir / "metadata.json",
+                json.dumps(
+                    convert_enum_to_name(
+                        {
+                            i: v
+                            for i, v in dataclasses.asdict(release).items()
+                            if i not in ["assets", "tickets_raw", "ap_ticket", "cryptex_tickets", "darwin_init"]
+                        }
+                        | {
+                            "tickets": {
+                                "os": hashlib.sha256(release.ap_ticket).hexdigest(),
+                                "cryptexes": [hashlib.sha256(x).hexdigest() for x in release.cryptex_tickets],
+                            }
+                        }
+                    ),
+                    indent=4,
+                    cls=ReleaseEncoder,
+                ),
+            )
+            if release.assets:
+                write(release_dir / "assets.json", json.dumps(convert_enum_to_name(release.assets), indent=4, cls=ReleaseEncoder))
+            if release.darwin_init:
+                write(release_dir / "darwin_init.json", json.dumps(release.darwin_init, indent=4, cls=ReleaseEncoder))
+            write(release_dir / "tickets_raw.der", release.tickets_raw)
+            write(release_dir / "apticket.der", release.ap_ticket)
+
+            cryptex_tickets_dir = release_dir / "cryptex_tickets"
+            for i, ticket in enumerate(release.cryptex_tickets):
+                write(cryptex_tickets_dir / f"cryptex_ticket_{i}.der", ticket)
+
+        if release.release_metadata_present:
+            assert release.created
+            write(
+                release_dir / "release-metadata.json",
+                json.dumps(
+                    {
+                        "assets": convert_enum_to_name(release.assets),
+                        "darwinInit": release.darwin_init,
+                        "schemaVersion": convert_enum_to_name(release.schema),
+                        "timestamp": release.created.replace(microsecond=0).strftime("%Y-%m-%dT%H:%M:%SZ"),
+                    },
+                    indent=2,
+                    sort_keys=True,
+                    cls=ReleaseEncoder,
+                ),
+            )
